cadbd6ff69d27079f8d0085f5b6ffcad50f8cc55
mentor-prime <mentor.prime.education@gmail.com>
2026-02-26 23:26:05 -0600

    added getSecondProduct

---
+++ src/test/java/tests/Products/getSecondProduct.java
@@ -0,0 +1,31 @@
+package tests.Products;
+
+import org.example.base.TestBase;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+
+public class getSecondProduct extends TestBase {
+    @Test
+    void getSingleProduct_shouldReturnProduct1() {
+
+        var response = given()
+                .spec(spec)
+                .when()
+                .get("/products/2")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(2))
+                .body("title", not(isEmptyOrNullString()))
+                .body("price", greaterThan(0f))
+                .body("category", not(isEmptyOrNullString()))
+                .extract()
+                .response();
+
+        System.out.println("=== PRODUCT ===");
+        System.out.println("ID: " + response.jsonPath().getInt("id"));
+        System.out.println("TITLE: " + response.jsonPath().getString("title"));
+    }
+}
